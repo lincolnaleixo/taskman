25fbcb1c1f7afaafebdc10a6da6ee6ea241f884e
lincoln <sleeved.quills.0u@icloud.com>
2023-01-16 11:06:15 +0100

    Initial Commit

---
+++ taskman/taskmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskmanApp.swift
+//  taskman
+//
+//  Created by robot on 2023-01-16.
+//
+
+import SwiftUI
+
+@main
+struct taskmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ taskmanUITests/taskmanUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  taskmanUITestsLaunchTests.swift
+//  taskmanUITests
+//
+//  Created by robot on 2023-01-16.
+//
+
+import XCTest
+
+final class taskmanUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
